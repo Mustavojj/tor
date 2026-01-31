ef8e81c741c3f72daef47442fb91291d0e8f540f
Mustavojj <mostavojj@gmail.com>
2026-01-31 04:09:28 +0300

    Update telegram-bot.js

--- api/telegram-bot.js
+++ api/telegram-bot.js
@@ -2,7 +2,6 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
     
     try {
-        
         const userId = req.headers['x-user-id'];
         const telegramHash = req.headers['x-telegram-hash'];
         
